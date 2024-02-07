9973364413bb0e4d0dfd63abe499031ed5c03e4b
Radiatore2 <radiatore2@gmail.com>
2024-02-07 18:45:04 +0100

    Classi Base

---
+++ src/main/java/com/develhope/spring/AdminAtt.java
@@ -0,0 +1,57 @@
+package com.develhope.spring;
+
+public class AdminAtt {
+	private String name;
+	private String surname;
+	private Integer phoneNumber;
+	private String email;
+	private String password;
+
+	public AdminAtt(String name, String surname, Integer phoneNumber, String email, String password) {
+		this.name = name;
+		this.surname = surname;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Integer getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Integer phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
