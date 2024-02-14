32a3476aaa362af580187a1a19b02c083898569a
Radiatore2 <radiatore2@gmail.com>
2024-02-14 14:53:47 +0100

    Classi Base + DB Tabelle

--- src/main/java/com/develhope/spring/Vehicles.java
+++ src/main/java/com/develhope/spring/Vehicles.java
@@ -1,24 +1,46 @@
 package com.develhope.spring;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Vehicles {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, name = "Tipo di veicolo")
 	private String type;
+	@Column(nullable = false, name = "Marchio")
 	private String brand;
+	@Column(nullable = false, name = "Modello")
 	private String model;
+	@Column(nullable = false, name = "Cilindrata")
 	private Integer displacement;
+	@Column(nullable = false, name = "Colorazione")
 	private String color;
-	private Integer power;
+	@Column(nullable = false, name = "Potenza")
+	private String power;
+	@Column(nullable = false, name = "Trasmissione")
 	private String transmissionType;
+	@Column(nullable = false, name = "Anno di registrazione")
 	private Integer yearOfRegistration;
+	@Column(nullable = false, name = "Carburante")
 	private String fuelType;
+	@Column(nullable = false, name = "Prezzo")
 	private int price;
+	@Column(nullable = false, name = "Prezzo scontato")
 	private int discountPrice;
+	@Column(nullable = false, name = "Accessori")
 	private String accessories;
+	@Column(nullable = false, name = "Condizioni")
 	private boolean condition;
+	@Column(nullable = false, name = "Disponibilità")
 	private boolean available;
 
-	public Vehicles(String type, String brand, String model, Integer displacement, String color, Integer power, String transmissionType,
+	public Vehicles(Long id, String type, String brand, String model, Integer displacement, String color, String power, String transmissionType,
 	                Integer yearOfRegistration, String fuelType, int price, int discountPrice, String accessories, boolean condition, boolean available) {
+		this.id = id;
 		this.type = type;
 		this.brand = brand;
 		this.model = model;
@@ -34,6 +56,15 @@ public class Vehicles {
 		this.condition = condition;
 		this.available = available;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getType() {
 		return type;
 	}
@@ -74,11 +105,11 @@ public class Vehicles {
 		this.color = color;
 	}
 
-	public Integer getPower() {
+	public String getPower() {
 		return power;
 	}
 
-	public void setPower(Integer power) {
+	public void setPower(String power) {
 		this.power = power;
 	}
 
