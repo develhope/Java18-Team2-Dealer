860045e4f1c74e381b0d718a2606f7d3cae4cb11
Radiatore2 <radiatore2@gmail.com>
2024-02-14 15:16:13 +0100

    Classi Base + DB Tabelle

--- src/main/java/com/develhope/spring/Customer.java
+++ src/main/java/com/develhope/spring/Customer.java
@@ -1,4 +1,39 @@
 package com.develhope.spring;
 
 public class Customer {
+	//Creare un ordine a partire da un veicolo contrassegnato come ordinabile
+	//      ------ METODO------
+
+	//Vedere i propri ordini
+	//      ------ METODO------
+
+	//Cancellare un ordine
+	//      ------ METODO------
+
+	//Creare un acquisto a partire da un veicolo contrassegnato come acquistabile
+	//      ------ METODO------
+
+	//Vedere i propri acquisti
+	//      ------ METODO------
+
+	//Creare un noleggio
+	//      ------ METODO------
+
+	//Vedere i propri noleggi
+	//      ------ METODO------
+
+	//Cancellare un noleggio
+	//      ------ METODO------
+
+	//Cancellare la propria utenza
+	//      ------ METODO------
+
+	//Modificare i dati dell’utente
+	//      ------ METODO------
+
+	//Ricercare un veicolo secondo diversi criteri (prezzo, colore, marca, modello, ecc)
+	//      ------ METODO------
+
+	//Ottenere i dettagli di un veicolo specifico
+	//      ------ METODO------
 }
