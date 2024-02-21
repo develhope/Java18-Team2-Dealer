7be49fb9143f292103aec143b81aa80ce200f326
Federico Maiello <federicomaiello@hotmail.it>
2024-02-21 09:14:10 +0100

    classi complete

--- src/main/java/com/develhope/spring/Application.java
+++ src/main/java/com/develhope/spring/Application.java
@@ -2,10 +2,11 @@ package com.develhope.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
