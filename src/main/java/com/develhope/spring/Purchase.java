61dcc91b8bab51554bdeb680a3510416c6440723
carmentecchia <carmentecchia@gmail.com>
2024-02-23 14:13:22 +0100

    Purchase e rent

---
+++ src/main/java/com/develhope/spring/Purchase.java
@@ -0,0 +1,8 @@
+package com.develhope.spring;
+
+public class Purchase {
+    int deposit;
+    boolean payed;
+    String status;
+    String orderOrSell;
+}
---
+++ src/main/java/com/develhope/spring/Rent.java
@@ -0,0 +1,12 @@
+package com.develhope.spring;
+
+import java.util.Date;
+
+public class Rent {
+Date startRent;
+Date endRent;
+double dailyRent;
+double totalRent;
+boolean payed;
+String rentedVehicle;
+}
