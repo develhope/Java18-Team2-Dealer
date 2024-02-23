fc528b4fe62a9d1f28885e721980f2fe92fcc4e8
Radiatore2 <radiatore2@gmail.com>
2024-02-23 14:16:14 +0100

    Classi Base +

--- src/main/java/com/develhope/spring/Purchase.java
+++ src/main/java/com/develhope/spring/Purchase.java
@@ -5,4 +5,43 @@ public class Purchase {
     boolean payed;
     String status;
     String orderOrSell;
+
+    public Purchase(int deposit, boolean payed, String status, String orderOrSell) {
+        this.deposit = deposit;
+        this.payed = payed;
+        this.status = status;
+        this.orderOrSell = orderOrSell;
+    }
+
+    public int getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(int deposit) {
+        this.deposit = deposit;
+    }
+
+    public boolean isPayed() {
+        return payed;
+    }
+
+    public void setPayed(boolean payed) {
+        this.payed = payed;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getOrderOrSell() {
+        return orderOrSell;
+    }
+
+    public void setOrderOrSell(String orderOrSell) {
+        this.orderOrSell = orderOrSell;
+    }
 }
