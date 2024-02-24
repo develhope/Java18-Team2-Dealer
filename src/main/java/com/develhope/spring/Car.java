1ac8d6e33e555a70dbb02ea7cf06b8da0e9f91f5
Radiatore2 <radiatore2@gmail.com>
2024-02-24 17:22:14 +0100

    Classi Base + lombok

--- src/main/java/com/develhope/spring/Car.java
+++ src/main/java/com/develhope/spring/Car.java
@@ -1,7 +1,7 @@
 package com.develhope.spring;
 
 public class Car extends Vehicles{
-	public Car(Long id, String type,String brand, String model, Integer displacement, String color, String power, String transmissionType, Integer yearOfRegistration, String fuelType, int price, int discountPrice, String accessories, boolean condition, boolean available) {
+	public Car(Long id, String type,String brand, String model, Integer displacement, String color, String power, String transmissionType, Integer yearOfRegistration, String fuelType, int price, int discountPrice, String accessories, String condition, boolean available) {
 		super(id, type ,brand, model, displacement, color, power, transmissionType, yearOfRegistration, fuelType, price, discountPrice, accessories, condition, available);
 	}
 }
--- src/main/java/com/develhope/spring/Vehicles.java
+++ src/main/java/com/develhope/spring/Vehicles.java
@@ -1,9 +1,16 @@
 package com.develhope.spring;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Vehicles {
 
 	@Id
@@ -37,164 +44,4 @@ public class Vehicles {
 	private String condition;
 	@Column(nullable = false, name = "disponibilità")
 	private boolean available;
-
-	public Vehicles(Long id, String type, String brand, String model, Integer displacement, String color, String power, String transmissionType,
-	                Integer yearOfRegistration, String fuelType, double price, double discountPrice, String accessories, String condition, boolean available) {
-		this.id = id;
-		this.type = type;
-		this.brand = brand;
-		this.model = model;
-		this.displacement = displacement;
-		this.color = color;
-		this.power = power;
-		this.transmissionType = transmissionType;
-		this.yearOfRegistration = yearOfRegistration;
-		this.fuelType = fuelType;
-		this.price = price;
-		this.discountPrice = discountPrice;
-		this.accessories = accessories;
-		this.condition = condition;
-		this.available = available;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getBrand() {
-		return brand;
-	}
-
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public Integer getDisplacement() {
-		return displacement;
-	}
-
-	public void setDisplacement(Integer displacement) {
-		this.displacement = displacement;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public String getPower() {
-		return power;
-	}
-
-	public void setPower(String power) {
-		this.power = power;
-	}
-
-	public String getTransmissionType() {
-		return transmissionType;
-	}
-
-	public void setTransmissionType(String transmissionType) {
-		this.transmissionType = transmissionType;
-	}
-
-	public Integer getYearOfRegistration() {
-		return yearOfRegistration;
-	}
-
-	public void setYearOfRegistration(Integer yearOfRegistration) {
-		this.yearOfRegistration = yearOfRegistration;
-	}
-
-	public String getFuelType() {
-		return fuelType;
-	}
-
-	public void setFuelType(String fuelType) {
-		this.fuelType = fuelType;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public double getDiscountPrice() {
-		return discountPrice;
-	}
-
-	public void setDiscountPrice(int discountPrice) {
-		this.discountPrice = discountPrice;
-	}
-
-	public String getAccessories() {
-		return accessories;
-	}
-
-	public void setAccessories(String accessories) {
-		this.accessories = accessories;
-	}
-
-	public String isCondition() {
-		return condition;
-	}
-
-	public String setCondition(String condition) {
-		this.condition = condition;
-		return condition;
-	}
-
-	public boolean isAvailable() {
-		return available;
-	}
-
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
-
-	@Override
-	public String toString() {
-		return "Vehicles{" +
-				"type='" + type + '\'' +
-				", brand='" + brand + '\'' +
-				", model='" + model + '\'' +
-				", displacement=" + displacement +
-				", color='" + color + '\'' +
-				", power=" + power +
-				", transmissionType='" + transmissionType + '\'' +
-				", yearOfRegistration=" + yearOfRegistration +
-				", fuelType='" + fuelType + '\'' +
-				", price=" + price +
-				", discountPrice=" + discountPrice +
-				", accessories='" + accessories + '\'' +
-				", condition=" + condition +
-				", available=" + available +
-				'}';
-	}
 }
