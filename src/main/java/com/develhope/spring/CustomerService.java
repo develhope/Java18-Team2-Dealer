46f11c05e4992f176c517dd050091c6948158d45
carmentecchia <carmentecchia@gmail.com>
2024-02-26 19:17:55 +0100

    creazione classi service

--- src/main/java/com/develhope/spring/CustomerService.java
+++ src/main/java/com/develhope/spring/CustomerService.java
@@ -1,4 +1,53 @@
 package com.develhope.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+    @Autowired
+    private AdminRepository adminRepository;
+    private CustomerRepository customerRepository;
+    private SellerRepository sellerRepository;
+    private VehiclesRepository vehiclesRepository;
+    private PurchaseRepository purchaseRepository;
+    private RentRepository rentRepository;
+    private OrderRepository orderRepository;
+
+    //Creare un ordine a partire da un veicolo contrassegnato come ordinabile (Customer)
+    //      ------ METODO------
+
+    //Vedere i propri ordini (Customer)
+    //      ------ METODO------
+
+    //Cancellare un ordine (Customer)
+    //      ------ METODO------
+
+    //Creare un acquisto a partire da un veicolo contrassegnato come acquistabile (Customer)
+    //      ------ METODO------
+
+    //Vedere i propri acquisti (Customer)
+    //      ------ METODO------
+
+    //Creare un noleggio (Customer)
+    //      ------ METODO------
+
+    //Vedere i propri noleggi (Customer)
+    //      ------ METODO------
+
+    //Cancellare un noleggio (Customer)
+    //      ------ METODO------
+
+    //Cancellare la propria utenza (Customer)
+    //      ------ METODO------
+
+    //Modificare i dati dell’utente (Customer)
+    //      ------ METODO------
+
+    //Ricercare un veicolo secondo diversi criteri (prezzo, colore, marca, modello, ecc) (Customer)
+    //      ------ METODO------
+
+    //Ottenere i dettagli di un veicolo specifico (Customer)
+    //      ------ METODO------
+
 }
--- src/main/java/com/develhope/spring/SellerService.java
+++ src/main/java/com/develhope/spring/SellerService.java
@@ -1,4 +1,38 @@
 package com.develhope.spring;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SellerService {
+    //Dettagli veicolo (Seller)
+    //      ------ METODO------
+
+    //Creazione ordine veicolo disponibile (Seller)
+    //      ------ METODO------
+
+    //Cancellazione ordine creato (Seller)
+    //      ------ METODO------
+
+    //Modificare ordine creato (Seller)
+    //      ------ METODO------
+
+    //Verifica stato ordine (Seller)
+    //      ------ METODO------
+
+    //Aggiorna stato ordine (Seller)
+    //      ------ METODO------
+
+    //Verifica ordini se disponibile o no (Seller)
+    //      ------ METODO------
+
+    //Creare un noleggio (Seller)
+    //      ------ METODO------
+
+    //Cancellare un noleggio (Seller)
+    //      ------ METODO------
+
+    //Modificare un noleggio (Seller)
+    //      ------ METODO------
+
+
 }
