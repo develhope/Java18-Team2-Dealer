501f254c24cad37ad536003e99bcfba914de3afe
Federico Maiello <federicomaiello@hotmail.it>
2024-03-14 14:44:37 +0100

    update controller Order

--- src/main/java/com/develhope/spring/Order/OrderController.java
+++ src/main/java/com/develhope/spring/Order/OrderController.java
@@ -1,7 +1,56 @@
 package com.develhope.spring.Order;
 
+import com.develhope.spring.Vehicles.EntityofVehicles.Vehicles;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/createIfIsAvaible")
+    public Vehicles createOrderIfIsAvailable(@PathVariable Long id, Vehicles vehicles){
+        return orderService.createOrderIfIsAvailable(id, vehicles);
+    }
+    @PostMapping("/createFotCustomer")
+    public Vehicles createOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+        return orderService.createOrderForCustomers(id, orders);
+    }
+
+    @DeleteMapping("/delete")
+    public Orders deleteOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+        return orderService.deleteOrderForCustomers(id, orders);
+    }
+
+    @PutMapping("/update")
+    public Orders updateOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+        return orderService.updateOrderForCustomers(id, orders);
+    }
+    @GetMapping("/allOrder")
+    public List<Orders> viewOrders(){
+        return orderService.viewOrders();
+    }
+
+    @GetMapping("/status/get")
+    public String getOrderStatus(@RequestBody Orders orders){
+        return orderService.getOrderStatus(orders);
+    }
+    @PutMapping("/status/update")
+    public Orders updateStatus(@RequestBody Orders orders, String status){
+        return orderService.updateStatus(orders, status);
+    }
+
+    @GetMapping("/isAvailable")
+    public boolean isAvailable(boolean isOrdered, @RequestBody Orders orders){
+        return orderService.isAvailable(isOrdered, orders);
+    }
+
+
 }
--- src/main/java/com/develhope/spring/Order/OrderService.java
+++ src/main/java/com/develhope/spring/Order/OrderService.java
@@ -3,10 +3,7 @@ package com.develhope.spring.Order;
 import com.develhope.spring.Vehicles.EntityofVehicles.Status;
 import com.develhope.spring.Vehicles.EntityofVehicles.Vehicles;
 import com.develhope.spring.Vehicles.VehiclesRepository;
-import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -15,7 +12,7 @@ public class OrderService {
     private OrderRepository orderRepository;
     private VehiclesRepository vehiclesRepository;
     //Creare un ordine a partire da un veicolo contrassegnato come ordinabile (Seller, Customer)
-    public Vehicles createOrderIfIsAvailable(@PathVariable Long id, Vehicles vehicles){
+    public Vehicles createOrderIfIsAvailable(Long id, Vehicles vehicles){
         if (vehiclesRepository.existsById(id)){
             if(vehicles.getStatus() == Status.DISPONIBILE){
                 return vehicles;
@@ -23,7 +20,7 @@ public class OrderService {
         }return null;
     }
     //Creare un ordine per un utente (Admin)
-    public Vehicles createOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+    public Vehicles createOrderForCustomers(Long id, Orders orders){
         if (vehiclesRepository.existsById(id)){
             orderRepository.save(orders);
         }
@@ -31,7 +28,7 @@ public class OrderService {
     }
 
     //Cancellare un ordine per un utente (Admin, Seller, Customer)
-    public Orders deleteOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+    public Orders deleteOrderForCustomers(Long id, Orders orders){
         if (orderRepository.existsById(id)){
             orderRepository.delete(orders);
         }
@@ -39,7 +36,7 @@ public class OrderService {
     }
 
     //Modificare un ordine per un utente (Admin, Seller)
-    public Orders updateOrderForCustomers(@PathVariable Long id, @RequestBody Orders orders){
+    public Orders updateOrderForCustomers(Long id, Orders orders){
         if (orderRepository.existsById(id)){
             orderRepository.save(orders);
         }
@@ -59,8 +56,9 @@ public class OrderService {
 
 
     //Aggiorna stato ordine (Seller)
-   public void updateStatus(Orders orders, String status){
+   public Orders updateStatus(Orders orders, String status){
         orders.setStatus(status);
+       return orders;
    }
 
     //Verifica ordini se disponibile o no (Seller)
